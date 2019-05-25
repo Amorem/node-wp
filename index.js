120492ca5851054668c5ff3cc7e2e7c4f303c2eb
AMOREM <amorem@amorem.com>
2019-05-25 11:59:08 +0200

    Add dotenv to store WP auth

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var WPAPI = require("wpapi");
+require("dotenv").config();
 var wp = new WPAPI({ endpoint: "https://comidoc.com/wp-json" });
 
-const start = async () => {
+const discover = async () => {
   const apiPromise = await WPAPI.discover("https://comidoc.com");
   console.log(apiPromise);
 };
 
-start();
+const post = async () => {
+  const rest = await wp.posts().create({
+    // "title" and "content" are the only required properties
+    title: "25 may 2019 : Udemy Free courses & coupons",
+    content: "Your post content",
+    // Post will be created as a draft by default if a specific "status"
+    // is not specified
+    status: "publish"
+  });
+  console.log(res);
+};
+
+console.log(process.env.WP_LOGIN, process.env.WP_PASSWORD);
+//post();
